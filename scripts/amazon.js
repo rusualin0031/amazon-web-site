a9dc48a35be01d7843d1cdeb4ec90d66aaed5304
rusualin0031 <123866644+rusualin0031@users.noreply.github.com>
2023-12-07 21:56:03 +0100

    7 dec

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -3,7 +3,7 @@ const products = [
     image: "images/products/athletic-cotton-socks-6-pairs.jpg",
     name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
     rating: {
-      starts: 4.5,
+      stars: 4.5,
       count: 87,
     },
     priceCents: 1090,
@@ -12,7 +12,7 @@ const products = [
     image: "images/products/intermediate-composite-basketball.jpg",
     name: "Intermediate Size Basketball",
     rating: {
-      starts: 4,
+      stars: 4,
       count: 127,
     },
     priceCents: 2095,
@@ -21,15 +21,17 @@ const products = [
     image: "images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg",
     name: "Adults Plain Cotton T-Shirt - 2 Pack",
     rating: {
-      starts: 4.5,
+      stars: 4.5,
       count: 56,
     },
     priceCents: 799,
   },
 ];
 
+let productsHTML = "";
+
 products.forEach((product) => {
-  const html = `
+  productsHTML += `
     <div class="product-container">
         <div class="product-image-container">
         <img class="product-image"
@@ -37,19 +39,19 @@ products.forEach((product) => {
         </div>
 
         <div class="product-name limit-text-to-2-lines">
-        Black and Gray Athletic Cotton Socks - 6 Pairs
+        ${product.name}
         </div>
 
         <div class="product-rating-container">
         <img class="product-rating-stars"
-            src="images/ratings/rating-45.png">
+            src="images/ratings/rating-${product.rating.stars * 10}.png">
         <div class="product-rating-count link-primary">
-            87
+            ${product.rating.count}
         </div>
         </div>
 
         <div class="product-price">
-        $10.90
+        ${product.priceCents / 100}
         </div>
 
         <div class="product-quantity-container">
@@ -78,7 +80,9 @@ products.forEach((product) => {
         Add to Cart
         </button>
     </div>
-    `;
-
-    console.log(html);
+  `;
 });
+
+console.log(productsHTML);
+
+document.querySelector('.js-products-grid').innerHTML = productsHTML;
