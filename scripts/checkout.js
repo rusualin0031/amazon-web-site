f29890d6b179be03c94c819a2f481a3db5a9f6b2
rusualin0031 <123866644+rusualin0031@users.noreply.github.com>
2023-12-14 20:44:43 +0100

    Regenerate HTML When updating option

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -6,9 +6,11 @@ import { deliveryOptions } from "../data/deliveryOptions.js";
 
 const today = dayjs();
 const deliveryDate = today.add(7, "days");
-deliveryDate.format("dddd, MMMM D");
+console.group(deliveryDate.format("dddd, MMMM D"));
 
-let cartSummaryHTML = "";
+function renderOrderSummary() {
+
+let cartSummaryHTML = '';
 
 cart.forEach((cartItem) => {
   const productId = cartItem.productId;
@@ -144,5 +146,9 @@ document.querySelectorAll('.js-delivery-option')
         element.addEventListener('click', () => {
             const {productId, deliveryOptionId} = element.dataset;
             updateDeliveryOption(productId, deliveryOptionId);
+            renderOrderSummary();
         });
     });
+}
+
+renderOrderSummary();
